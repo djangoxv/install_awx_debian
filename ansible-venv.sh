621fc5d53cf85cc4bc7e5f52453216bacc039ff3
Jeremy Capps <djangoxv@gmail.com>
2019-08-16 21:58:21 -0700

    Updated virtualenv to pull submodules and path prompt

--- ansible-venv.sh
+++ ansible-venv.sh
@@ -3,8 +3,10 @@
 # Simple virtualenv setup for latest ansible
 # Requires python3 in path
 
+git submodule update --init --recursive
 python3 -m venv ./venv
 source venv/bin/activate
 pip install --upgrade pip
 pip install --upgrade ansible
-bash --rcfile venv/bin/activate
+export PS1=[`pwd`]
+bash --rcfile venv/bin/activate -i
